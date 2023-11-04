b5b7491592ed3f2a19eb98b9e0dece86d234a342
Kavinda Rochana <odc_kavindar@m1.com.sg>
2023-11-04 16:25:19 +0800

    Add routes n pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import About from './pages/About';
+import Logout from './pages/Logout';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-   <h1 className='text-red-500'>App</h1>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/logout' element={<Logout />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/profile' element={<Profile />}/>
+    </Routes>
+   </BrowserRouter>
   );
 }
