aef5c9860ecf4824be1823b66da8dcfa4f04ae55
Kavinda Rochana <odc_kavindar@m1.com.sg>
2023-11-04 19:02:26 +0800

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import About from './pages/About';
 import Logout from './pages/Logout';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
    <BrowserRouter>
+   <Header />
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/login' element={<Login />}/>
