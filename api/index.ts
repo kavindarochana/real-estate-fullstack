ca91bab536e41e8d46ca381f5202fbf4a6249f98
Kavinda Rochana <odc_kavindar@m1.com.sg>
2023-11-06 00:01:19 +0800

    Env and User schema added

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING).then(() => {
+    console.log('Connected to DB');
+}).catch((error) => {
+    console.log('DB Error ', error);
+});
 
 const app = express();
 
